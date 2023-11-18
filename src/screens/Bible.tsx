36e00bd4fd39f63ae8508bb3db801281a7d0736b
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-18 21:19:13 +0000

    feat: add loading component while fetch verses

--- src/screens/Bible.tsx
+++ src/screens/Bible.tsx
@@ -5,6 +5,7 @@ import { Select } from "../components/Select";
 import { fetchVerses } from "../remote/get-verses";
 import { SelectItem } from "../components/SelectItem";
 import { BIBLE_BOOKS } from "../constants/bible";
+import { Loading } from "../components/Loading";
 
 type Book = {
   id: string 
@@ -21,6 +22,7 @@ export function Bible () {
   const [chapters, setChapters] = useState<number>()
   const [selectedChapter, setSelectedChapter] = useState<string>()
   const [verses, setVerses] = useState<Verse[]>([])
+  const [isLoading, setIsLoading] = useState(false)
 
   async function handleSelectBook (bookId: string) {
     const foundBook = BIBLE_BOOKS.find((book) => book.id === bookId)
@@ -31,12 +33,15 @@ export function Bible () {
   }
 
   async function getVerses (chapter: string) {
+    setIsLoading(true)
     try {
       setSelectedChapter(chapter)
       const foundVerses = await fetchVerses(selectedBook?.id, chapter)
       setVerses(foundVerses?.verses)
     } catch (error) {
       Alert.alert('Não foi possível carregar os versículos')
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -83,7 +88,7 @@ export function Bible () {
         </Center>
 
         {
-          verses?.map((verse: Verse) => (
+          !isLoading && verses?.length > 0 ? verses.map((verse: Verse) => (
             <Text 
               mt={2}
               mb={2}
@@ -94,7 +99,11 @@ export function Bible () {
             >  
               {verse.number}. {verse.text}
             </Text>
-          ))
+          )) : (
+            <Center flex={1}>
+              <Loading />
+            </Center>
+          )
         }
 
       </VStack>
