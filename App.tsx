51f35a16a9e67b46e9dfb3e1f81abebee2a6ed7c
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-18 15:11:55 +0000

    feat: add custom style to app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from 'react-native';
+import { Center, NativeBaseProvider, Text } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <NativeBaseProvider>
+      <Center>
+        {
+          fontsLoaded && <Text color="gray.700" fontFamily="heading">Hello World</Text> 
+        }
+      </Center>
+    </NativeBaseProvider>
   );
 }
