30467e7c51b9735f8dac09346987584be64660cf
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-18 15:17:44 +0000

    feat: create loading component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { Center, NativeBaseProvider, Text } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -9,11 +10,17 @@ export default function App() {
 
   return (
     <NativeBaseProvider>
-      <Center>
-        {
-          fontsLoaded && <Text color="gray.700" fontFamily="heading">Hello World</Text> 
-        }
-      </Center>
+      {
+        fontsLoaded ? 
+          <Text 
+            color="gray.700" 
+            fontFamily="heading"
+          >
+            Hello World
+          </Text>
+        :  
+        <Loading /> 
+      }
     </NativeBaseProvider>
   );
 }
