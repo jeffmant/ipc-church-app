d92095093c04908db66b2449475c44c27a0a90f8
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-18 16:22:48 +0000

    feat: create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+import { IButtonProps, Button as NativeBaseButton, Text } from 'native-base'
+
+type ButtonProps = IButtonProps & {
+  title: string
+  variant?: 'solid' | 'outline'
+}
+
+export function Button ({ title, variant = 'solid', ...rest }: ButtonProps) {
+  return (
+    <NativeBaseButton 
+      w="full"
+      h={16}
+      bg={variant === 'outline' ? "transparent" : "blue.700"}
+      borderWidth={variant === 'outline' ? 1 : 0}
+      borderColor="blue.500"
+      rounded="sm"
+      _pressed={{
+        bg: variant === 'outline' ? "gray.500" : "blue.500"
+      }}
+      {...rest}
+    >
+      <Text
+        color={variant === 'outline' ? "blue.500" : "white"}
+        fontFamily="heading"
+        fontSize="sm"
+      >
+        {title}
+      </Text>
+    </NativeBaseButton>
+  )
+}
