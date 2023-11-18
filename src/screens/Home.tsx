fe3783df2f3f5f2e6a59a90bf3b596fa138f2ba5
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-18 22:50:05 +0000

    feat: create activity component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,37 @@
-import { Center, Heading } from "native-base";
+import { VStack } from "native-base";
+import { Activity } from "../components/Activity";
+
+import serviceImage from '../assets/service.jpg'
+import cellImage from '../assets/cell.jpg'
+import ministryImage from '../assets/ministry.jpg'
+import eventImage from '../assets/event.jpg'
 
 export function Home () {
   return (
-    <Center flex={1}>
-      <Heading>Home</Heading>
-    </Center>
+    <VStack px={8} py={16}>
+      <Activity 
+        title="Cultos"
+        subtitle="Fique por dentro dos nossos cultos"
+        image={serviceImage}
+      />
+      
+      <Activity 
+        title="Células"
+        subtitle="Encontre uma célula próximo a você"
+        image={cellImage}
+      />
+
+      <Activity 
+        title="Ministérios"
+        subtitle="Conheça os ministérios da IPC"
+        image={ministryImage}
+      />
+
+      <Activity 
+        title="Eventos"
+        subtitle="Conheça os ministérios da IPC"
+        image={eventImage}
+      />
+    </VStack>
   )
 }
