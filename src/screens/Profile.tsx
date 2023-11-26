15c9b0c7f49b08decfa8e5ae1cfbc1bd93f2e630
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-26 17:22:39 +0000

    feat: use react hook form at profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,22 +1,31 @@
-import { Avatar, Center, Link, Skeleton, Text, VStack } from "native-base";
-import { Input } from "../components/Input";
 import { useClerk, useUser } from "@clerk/clerk-expo";
-import { useState } from "react";
-import { Button } from "../components/Button";
-import { Loading } from "../components/Loading";
-import * as ImagePicker from 'expo-image-picker';
-import { Alert, TouchableOpacity } from "react-native";
+import { yupResolver } from "@hookform/resolvers/yup";
 
+import * as ImagePicker from 'expo-image-picker';
+
+import { Avatar, Center, Link, ScrollView, Skeleton, Text, VStack, useToast } from "native-base";
+import { useState } from "react";
+import { Controller, Resolver, useForm } from "react-hook-form";
+import { TouchableOpacity } from "react-native";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
+import { ProfileDTO } from "../dtos/profile.dto";
+import { profileValidationSchema } from "../utils/validations/profile.schema";
 
 export function Profile () {
-  const { user, isLoaded, isSignedIn } = useUser()
+  const { user } = useUser()
   const { signOut } = useClerk();
 
-  const [name, setName] = useState(user?.fullName || '')
-  const [email, setEmail] = useState(user?.primaryEmailAddress?.emailAddress || '')
+  const { control, handleSubmit, setError, formState: { errors } } = useForm<ProfileDTO>({
+    defaultValues: {
+      name: user?.fullName || '',
+      email: user?.primaryEmailAddress?.emailAddress,
+    },
+    resolver: yupResolver(profileValidationSchema) as unknown as Resolver<ProfileDTO, any>
+  })
 
-  const [oldPassword, setOldPassword] = useState('')
-  const [newPassword, setNewPassword] = useState('')
+  const toast = useToast()
 
   const [isLoading, setIsLoading] = useState(false)
 
@@ -42,135 +51,193 @@ export function Profile () {
           file: 'data:image/jpeg;base64,' + uploadedImage.base64 
         })
 
-        Alert.alert("Foto atualizada!")
+        toast.show({
+          title: 'Foto atualizada!',
+          placement: 'top',
+          bgColor: 'green.500'
+        })
       }
     } catch (error: any) {
-      Alert.alert(error?.message || "Não foi possível trocar foto. Tente novamente.")
+      toast.show({
+        title: 'Não foi possível trocar foto. Tente novamente.',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
     } finally {
       setAvatarIsLoading(false)
     }
   };
 
-  async function handleUpdate () {
+  async function handleUpdate (data: ProfileDTO) {
     setIsLoading(true)
     try {
-      if (user?.fullName !== name) {
+      if (data.name && data.name !== user?.fullName) {
         await user?.update({
-          firstName: name.split(' ')[0],
-          lastName: name.split(' ')[1] || ''
+          firstName: data.name.split(' ')[0],
+          lastName: data.name.split(' ')[1] || ''
         })
-
-        Alert.alert("Informações atualizadas!")
       }
 
-      if (oldPassword && newPassword) {
+      if (data.oldPassword && data.password) {
         await user?.updatePassword({
-          currentPassword: oldPassword,
-          newPassword: newPassword
+          currentPassword: data.oldPassword,
+          newPassword: data.password
         })
-
-        setOldPassword('')
-        setNewPassword('')
-
-        Alert.alert("Senha atualizada!")
       }
+
+      toast.show({
+        title: 'Perfil atualizado!',
+        placement: 'top',
+        bgColor: 'green.500'
+      })
     } catch (error: any) {
-      Alert.alert(error?.message || 'Não foi possível atualizar. Tente novamente!')
+      toast.show({
+        title: 'Não foi possível atualizar. Tente novamente.',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
     } finally {
       setIsLoading(false)
     }
   }
 
   return (
-    <VStack px={10}>
-      <Center mt={16}>
-        {
-          avatarIsLoading ? (
-            <>
-              <Skeleton size="32" rounded="full" />
-              <Text
-                fontFamily="body"
-                color="gray.100"
-                mt={2}
-              >
-                Carregando...
-              </Text>
-            </>
-          ) :
-          (
-            <>
-              <TouchableOpacity onPress={uploadImage}>
-                <Avatar 
-                  source={{
-                    uri: user?.imageUrl
-                  }}
-                  size={"2xl"}
-                />
-              </TouchableOpacity>
-              <Link onPress={uploadImage}>
+    <ScrollView showsVerticalScrollIndicator={false} _contentContainerStyle={{ paddingBottom: 10 }}>
+      <VStack px={10}>
+        <Center mt={16}>
+          {
+            avatarIsLoading ? (
+              <>
+                <Skeleton size="32" rounded="full" />
                 <Text
                   fontFamily="body"
                   color="gray.100"
                   mt={2}
                 >
-                  Trocar Foto
+                  Carregando...
                 </Text>
-              </Link>
-            </>       
-          )
-        }
-      </Center>
+              </>
+            ) :
+            (
+              <>
+                <TouchableOpacity onPress={uploadImage}>
+                  <Avatar 
+                    source={{
+                      uri: user?.imageUrl
+                    }}
+                    size={"2xl"}
+                  />
+                </TouchableOpacity>
+                <Link onPress={uploadImage}>
+                  <Text
+                    fontFamily="body"
+                    color="gray.100"
+                    mt={2}
+                  >
+                    Trocar Foto
+                  </Text>
+                </Link>
+              </>       
+            )
+          }
+        </Center>
 
-      <Center mt={8}>
-        <Input 
-          placeholder="Nome"
-          value={name}
-          onChangeText={setName}
+        <Center mt={8}>
+          <Controller 
+            control={control}
+            name="name"
+            render={({ field: { value, onChange } }) => (
+              <Input 
+                placeholder="Nome"
+                autoCapitalize="words"
+                value={value}
+                onChangeText={onChange}
+                errorMessage={errors?.name?.message}
+              />
+            )}
+          />
+
+          <Controller 
+            control={control}
+            name="email"
+            render={({ field: { value } }) => (
+              <Input 
+                placeholder="Email"
+                value={value}
+                isDisabled
+              />
+            )}
+          />
+        </Center>
+
+        <Center mt={4}>
+          <Text
+            color="gray.100"
+            fontFamily="body"
+            fontSize="md"
+            alignSelf="flex-start"
+            mb={4}
+          >
+            Alterar senha
+          </Text>
+
+          <Controller 
+            control={control}
+            name="oldPassword"
+            render={({ field: { value, onChange } }) => (
+              <Input 
+              placeholder="Senha antiga"
+              secureTextEntry
+                value={value}
+                onChangeText={onChange}
+                errorMessage={errors?.oldPassword?.message}
+              />
+            )}
+          />
+
+          <Controller 
+            control={control}
+            name="password"
+            render={({ field: { value, onChange } }) => (
+              <Input 
+                placeholder="Nova senha"
+                secureTextEntry
+                value={value}
+                onChangeText={onChange}
+                errorMessage={errors?.password?.message}
+              />
+            )}
+          />
+
+          <Controller 
+            control={control}
+            name="confirmPassword"
+            render={({ field: { value, onChange } }) => (
+              <Input 
+                placeholder="Confirmar senha"
+                secureTextEntry
+                value={value}
+                onChangeText={onChange}
+                errorMessage={errors?.confirmPassword?.message}
+              />
+            )}
+          />
+
+        </Center>
+
+        <Button 
+          mt={4} 
+          title="Atualizar" 
+          onPress={handleSubmit(handleUpdate)}
+          isLoading={isLoading}
         />
-
-        <Input 
-          placeholder="Email"
-          value={email}
-          isDisabled
+        <Button 
+          mt={2} 
+          variant={"outline"} 
+          title={"Sair"}
+          onPress={handleLogout} 
         />
-      </Center>
-
-      <Center mt={4}>
-        <Text
-          color="gray.100"
-          fontFamily="body"
-          fontSize="md"
-          alignSelf="flex-start"
-          mb={4}
-        >
-          Alterar senha
-        </Text>
-
-        <Input 
-          placeholder="Senha antiga"
-          value={oldPassword}
-          onChangeText={setOldPassword}
-          secureTextEntry
-        />
-
-        <Input 
-          placeholder="Nova senha"
-          value={newPassword}
-          onChangeText={setNewPassword}
-          secureTextEntry
-        />
-      </Center>
-
-      <Button 
-        mt={4} 
-        title={isLoading ? <Loading /> : "Atualizar"} 
-        onPress={handleUpdate}
-      />
-      <Button 
-        variant={"outline"} 
-        title={"Sair"}
-        onPress={handleLogout} 
-      />
-    </VStack>
+      </VStack>
+    </ScrollView>
   )
 }
