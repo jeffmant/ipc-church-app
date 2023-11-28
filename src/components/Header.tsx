bc435e83cb9650e2eebd61465236dc63163cf90a
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-11-28 01:30:08 +0000

    refactor: header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,28 +8,25 @@ type HeaderProps = {
 
 export function Header ({ title, handleGoBack }: HeaderProps) {
   return (
-    <HStack bg="gray.600" pb={6} pt={16} justifyContent="space-around">
+    <HStack bg="gray.600" pb={6} pt={16} px={5} justifyContent="space-between">
       <Box>
-          <TouchableOpacity onPress={handleGoBack}>
-            <HStack justifyContent="center" alignItems="center" ml={1}>
-              <ChevronLeftIcon color="gray.100" />
-              <Heading
-                fontFamily="heading"
-                fontSize="md"
-                color="gray.100"
-                ml={1}
-              >
-                Voltar
-              </Heading>
-            </HStack>
-          </TouchableOpacity>
-        </Box>
+        <TouchableOpacity onPress={handleGoBack}>
+          <HStack justifyContent="center" alignItems="center" ml={1}>
+            <ChevronLeftIcon color="gray.100" />
+            <Heading
+              fontFamily="heading"
+              fontSize="md"
+              color="gray.100"
+              ml={1}
+            >
+              Voltar
+            </Heading>
+          </HStack>
+        </TouchableOpacity>
+      </Box>
       <Heading color="gray.100" fontSize="xl" fontFamily="heading">
         { title }
       </Heading>
-      <Box />
-      <Box />
-      <Box />
     </HStack>
   )
 }
